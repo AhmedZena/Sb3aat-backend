3920e1b43cf095c4a1728627617b74f2ba718a50
ahmed zena <ahmedzena53@gmail.com>
2024-03-30 08:33:15 +0200

    searching enhance

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,7 +1,7 @@
 // search service or course by part of title in params
 const serviceModel = require("../models/serviceModel");
 const courseModel = require("../models/courses");
-
+const subCategoryModel = require("../models/subCategories");
 // Search for a service and course by part of its title
 const searchServiceAndCourse = (req, res) => {
   const searchQuery = req.params.search;
@@ -55,4 +55,79 @@ const searchCourse = (req, res) => {
     });
 };
 
-module.exports = { searchServiceAndCourse, searchService, searchCourse };
+// search service title in subcategory
+const searchServiceInSubCategory = (req, res) => {
+  const searchQuery = req.params.search;
+  const subCategoryID = req.params.subCategoryID;
+
+  serviceModel
+    .find({
+      title: { $regex: searchQuery, $options: "i" },
+      subCategoryID,
+    })
+    .then((services) => {
+      res.status(200).json(services);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ error: "Error retrieving data", details: error.message });
+    });
+};
+
+// search course title in subcategory
+const searchCourseInSubCategory = (req, res) => {
+  const searchQuery = req.params.search;
+  const subCategoryID = req.params.subCategoryID;
+
+  courseModel
+    .find({
+      title: { $regex: searchQuery, $options: "i" },
+      subCategoryID,
+    })
+    .then((courses) => {
+      res.status(200).json(courses);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ error: "Error retrieving data", details: error.message });
+    });
+};
+
+// search both service and course in subcategory
+const searchServiceAndCourseInSubCategory = (req, res) => {
+  const searchQuery = req.params.search;
+  const subCategoryID = req.params.subCategoryID;
+
+  // Execute both searches in parallel
+  Promise.all([
+    serviceModel.find({
+      title: { $regex: searchQuery, $options: "i" },
+      subCategoryID,
+    }),
+    courseModel.find({
+      title: { $regex: searchQuery, $options: "i" },
+      subCategoryID,
+    }),
+  ])
+    .then(([services, courses]) => {
+      // Combine the results
+      const combinedResults = { services, courses };
+      res.status(200).json(combinedResults);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ error: "Error retrieving data", details: error.message });
+    });
+};
+
+module.exports = {
+  searchServiceAndCourse,
+  searchService,
+  searchCourse,
+  searchServiceInSubCategory,
+  searchCourseInSubCategory,
+  searchServiceAndCourseInSubCategory,
+};
--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -4,6 +4,9 @@ const {
   searchServiceAndCourse,
   searchService,
   searchCourse,
+  searchServiceInSubCategory,
+  searchCourseInSubCategory,
+  searchServiceAndCourseInSubCategory,
 } = require("../controllers/searchController");
 
 // search for a service or course by part of its title
@@ -14,5 +17,16 @@ router.get("/service/:search", searchService);
 
 // search for a course by part of its title
 router.get("/course/:search", searchCourse);
+
+// search for a service by part of its title in a subcategory
+
+// search service title in subcategory
+router.get("/service/:search/:subCategoryID", searchServiceInSubCategory);
+
+// search course title in subcategory
+router.get("/course/:search/:subCategoryID", searchCourseInSubCategory);
+
+// search for a service or course by part of its title in a subcategory
+router.get("/:search/:subCategoryID", searchServiceAndCourseInSubCategory);
 
 module.exports = router;
